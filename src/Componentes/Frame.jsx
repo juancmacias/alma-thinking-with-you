d81934e9a702bfa0c3595ce59eaa6864cedb4a12
juan carlos macias <juancmaciassalvador@gmail.com>
2023-08-24 15:35:01 +0200

    Frame

---
+++ src/Componentes/Frame.jsx
@@ -0,0 +1,11 @@
+function Frame() {
+    return (
+      <div className="frame">         
+          <p>
+            Para mostrar el A-Frame
+          </p>        
+      </div>
+    );
+  }
+  
+  export default Frame;
---
+++ src/Componentes/Inicial.jsx
@@ -0,0 +1,9 @@
+function Inicial() {
+    return (
+          <p>
+           
+          </p>        
+    );
+  }
+  
+  export default Inicial;
